725a549a988a36537192fffaede26c1108aecaf8
Ekaterina Eliseeva <katerina17778@gmail.com>
2025-04-09 21:24:07 +0300

    chng puppeteer options

--- src/modules/wizz/api.ts
+++ src/modules/wizz/api.ts
@@ -22,9 +22,14 @@ export async function getWizzairSearchHeaders(
     browser = await puppeteer.launch({
         headless: true,
         args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage'
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
         ],
     });
     
